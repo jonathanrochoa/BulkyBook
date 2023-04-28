c1d0283d3a31b22b02b809f33d6f63907f2399ce
jonathanochoasurya <81263926+jonathanochoasurya@users.noreply.github.com>
2023-04-27 21:34:09 -0400

    Add migrations and apply to database

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -1,15 +1,25 @@
 ﻿using BulkyBookWeb.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
-namespace BulkyBookWeb.Data
+namespace BulkyBookWeb.Data;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
         {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Bulky;Trusted_Connection=True;");
 
+            return new ApplicationDbContext(optionsBuilder.Options);
         }
-
-        public DbSet<Category> Categories { get; set; }
     }
 }
